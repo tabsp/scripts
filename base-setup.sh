06d0ebc6237edcab208a449167ea0cc296563f34
tabsp <tabsp@qq.com>
2022-02-19 17:53:41 +0800

    updated

--- base-setup.sh
+++ base-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm git base-devel xorg-apps xorg-server xorg-xinit neovim tmux xsel ttf-joypixels wqy-microhei fcitx5-im fcitx5-rime
+sudo pacman -S --noconfirm git base-devel xorg-apps xorg-server xorg-xinit neovim tmux xsel ttf-joypixels wqy-microhei ttf-jetbrains-mono fcitx5-im fcitx5-rime picom ninja ripgrep lazygit
 
 SUCKLESS_DIR=$HOME/suckless
 rm -rf $SUCKLESS_DIR
@@ -17,6 +17,7 @@ cd $SUCKLESS_DIR/dmenu && sudo make clean install
 cd $SUCKLESS_DIR/slstatus && sudo make clean install
 
 echo "exec slstatus &" >> $HOME/.xinitrc
+echo "exec picom &" >> $HOME/.xinitrc
 echo "exec fcitx5 &" >> $HOME/.xinitrc
 echo "exec dwm" >> $HOME/.xinitrc
 
@@ -39,4 +40,5 @@ yay -S --noconfirm nerd-fonts-jetbrains-mono
 
 rm -rf $HOME/dotfiles && git clone https://github.com/tabsp/dotfiles.git $HOME/dotfiles
 rm -rf $HOME/.config && ln -s $HOME/dotfiles/.config $HOME/.config
+rm -rf $HOME/.zsh && rm -f $HOME/.zshrc && ln -s $HOME/dotfiles/.zsh $HOME/.zsh && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm -f .tmux.conf && ln -s -f $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
