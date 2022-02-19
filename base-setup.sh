29b7a6358693df334f5da4e0650ab6fda6748f0b
tabsp <tabsp@qq.com>
2022-02-20 00:03:48 +0800

    updated

--- base-setup.sh
+++ base-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm git base-devel xorg-apps xorg-server xorg-xinit neovim tmux xsel ttf-joypixels wqy-microhei ttf-jetbrains-mono fcitx5-im fcitx5-rime picom ninja ripgrep lazygit ranger fd fzf bat man-db
+sudo pacman -S --noconfirm git base-devel xorg-apps xorg-server xorg-xinit neovim tmux xsel ttf-joypixels wqy-microhei ttf-jetbrains-mono fcitx5-im fcitx5-rime ninja ripgrep lazygit ranger fd fzf bat man-db alacritty neofetch
 
 SUCKLESS_DIR=$HOME/suckless
 rm -rf $SUCKLESS_DIR
@@ -36,9 +36,10 @@ git clone https://aur.archlinux.org/yay.git $YAY_DIR
 cd $YAY_DIR
 makepkg -si --noconfirm
 
-yay -S --noconfirm nerd-fonts-jetbrains-mono
+yay -S --noconfirm nerd-fonts-jetbrains-mono picom-jonaburg-git
 
 rm -rf $HOME/dotfiles && git clone https://github.com/tabsp/dotfiles.git $HOME/dotfiles
 rm -rf $HOME/.config && ln -s $HOME/dotfiles/.config $HOME/.config
 rm -rf $HOME/.zsh && rm -f $HOME/.zshrc && ln -s $HOME/dotfiles/.zsh $HOME/.zsh && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm -f .tmux.conf && ln -s -f $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
